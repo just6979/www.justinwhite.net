d94ec50f3dba3788b5675ba513dc564b7df9e1ca
Justin White <just6979@gmail.com>
2012-04-27 03:19:44 -0400

    Added jQuery, added a mobile stylesheet for displays < 600px tall. On mobiles, section contents are hidden by default, and visibility is toggled by tapping on the header.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // will contain global site JS stuff
 
+$(document).ready(function(){
+    $("section header").click(function(event){
+        $(this).siblings('.content').toggleClass("visible invisible");
+    });
+});
